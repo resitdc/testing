b1637f3980b8e9cfaa8a916b0ed783291c89394c
Restu Dwi Cahyo <dwi.restu3@gmail.com>
2023-04-14 04:44:31 +0700

    Clean code container

--- src/components/atoms/Container/index.jsx
+++ src/components/atoms/Container/index.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import propTypes from "prop-types";
 
 const Container = (props) => {
   const className = ["container"];
+  if (props.default) className.push("mx-auto px-4 sm:px-6 lg:px-8");
   className.push(props.className);
   return (
     <div className={className.join(" ")}>
-      { children }
+      { props.children }
     </div>
   );
+};
+
+Container.propTypes = {
+  children: propTypes.node.isRequired,
+  className: propTypes.string,
+  default: propTypes.bool,
 };
 
 export default Container;
