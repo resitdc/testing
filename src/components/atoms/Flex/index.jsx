4c83e92441a62646ea817c401802f7c43d154353
Restu Dwi Cahyo <dwi.restu3@gmail.com>
2023-04-14 04:45:22 +0700

    Add prop types

--- src/components/atoms/Flex/index.jsx
+++ src/components/atoms/Flex/index.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import propTypes from "prop-types";
 
 const Flex = (props) => {
   const className = ["flex"];
   className.push(props.className);
   return (
     <div className={className.join(" ")}>
-      { children }
+      { props.children }
     </div>
   );
+};
+
+Flex.propTypes = {
+  children: propTypes.node.isRequired,
+  className: propTypes.string,
 };
 
 export default Flex;
