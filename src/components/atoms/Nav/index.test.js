b8273b1115e428c520876e8ef0028795e1f5a17b
Restu Dwi Cahyo <dwi.restu3@gmail.com>
2023-04-14 05:03:59 +0700

    Fix required testing

--- src/components/atoms/Nav/index.test.js
+++ src/components/atoms/Nav/index.test.js
@@ -15,7 +15,7 @@ describe("Nav component", () => {
   });
 
   it("should have test class name when className prop is provided", () => {
-    const { container } = render(<Nav className="test-class" />);
+    const { container } = render(<Nav className="test-class"><div>Test</div></Nav>);
     expect(container.firstChild).toHaveClass("test-class");
   });
 });
