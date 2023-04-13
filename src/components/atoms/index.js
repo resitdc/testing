9f5714905a83a88448cfbe5961d234c046d88691
Restu Dwi Cahyo <dwi.restu3@gmail.com>
2023-04-14 04:08:28 +0700

    Listing Container, Nav and Flex

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,1 +1,3 @@
-// SAMPLE FILE FOR GIT
+export { default as Container } from "components/atoms/Container";
+export { default as Nav } from "components/atoms/Nav";
+export { default as Flex } from "components/atoms/Flex";
