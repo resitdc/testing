dd065735c5495f0cefbed9f1eba2f95f0a88bc86
Restu Dwi Cahyo <dwi.restu3@gmail.com>
2023-04-14 04:59:28 +0700

    Add unit testing for Nav

---
+++ src/components/atoms/Nav/index.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Nav from "components/atoms/Nav";
+
+describe("Nav component", () => {
+  it("should render children", () => {
+    const { getByText } = render(
+      <Nav>
+        <div>Test 1</div>
+        <div>Test 2</div>
+      </Nav>
+    );
+    expect(getByText("Test 1")).toBeInTheDocument();
+    expect(getByText("Test 2")).toBeInTheDocument();
+  });
+
+  it("should have test class name when className prop is provided", () => {
+    const { container } = render(<Nav className="test-class" />);
+    expect(container.firstChild).toHaveClass("test-class");
+  });
+});
