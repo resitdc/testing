f9f2be5a42ba3242645d6ecd8df3825daa9ca4b7
Restu Dwi Cahyo <dwi.restu3@gmail.com>
2023-04-14 16:37:45 +0700

    Remove hash router

--- src/index.js
+++ src/index.js
@@ -6,16 +6,13 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "plugins/redux/store";
 import "assets/styles/main.scss";
-import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <HashRouter>
-          <App />
-        </HashRouter>
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>
