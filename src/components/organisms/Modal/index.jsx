9817e26c71620503c52fe47e76c227a92c834cba
Restu Dwi Cahyo <dwi.restu3@gmail.com>
2023-04-14 13:35:15 +0700

    Fix some code Modal

--- src/components/organisms/Modal/index.jsx
+++ src/components/organisms/Modal/index.jsx
@@ -6,20 +6,21 @@ import {
 } from "components/atoms";
 import { ModalHeader } from "components/molecules";
 
-const Modal = ({ isOpen, onClose, children }) => {
+const Modal = ({ isOpen, onClose, children, title }) => {
   return (
     <ModalOverlay isOpen={isOpen} onClick={onClose}>
       <ModalBox isOpen={isOpen}>
-        <ModalHeader title="Modal Title" onClose={onClose} />
+        <ModalHeader title={title} onClose={onClose} />
         <div className="px-4 py-2">{children}</div>
       </ModalBox>
     </ModalOverlay>
   );
 };
 
-ModalOverlay.propTypes = {
+Modal.propTypes = {
   children: propTypes.node.isRequired,
   isOpen: propTypes.bool,
+  title: propTypes.string,
   onClose: propTypes.func,
 };
 
